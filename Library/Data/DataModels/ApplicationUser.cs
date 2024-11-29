c12e61b7a6e655b6b02e3d483ba381f947680dbe
Ivaylo P Kalaydzhiev <160835946+IvoPetkov1985@users.noreply.github.com>
2024-11-29 18:25:48 +0200

    Delete Library directory

--- Library/Data/DataModels/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Library.Data.DataModels
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public IEnumerable<ApplicationUserBook> ApplicationUsersBooks { get; set; } = new List<ApplicationUserBook>();
-    }
-}
